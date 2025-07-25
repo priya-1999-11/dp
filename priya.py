255dac7022eed2189ef6fc255087a5807b246dbb
Shaik Arshadh kareem <shaikarshadhh@gmail.com>
2025-07-25 12:35:10 +0530

    Fixed submodule issue and added priya.py correctly

---
+++ priya.py
@@ -0,0 +1,15 @@
+# greet.py
+
+def greet(name):
+    print(f"Hello, {name}! Have a great day 😊")
+
+# Ask user for their name
+user_name = input("Enter your name: ")
+greet(user_name)
+print("have a good day")
+print('hiees')
+print('dppp')
+a=2
+b=3
+c=a+b
+print(c)
